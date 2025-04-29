b853e6d0d2fc480b93911faa24f527df26040515
Rahul <rahulslg22@gmail.com>
2025-04-29 14:19:05 +0530

    My Second commit

--- src/test/java/sampleProject/TestProject1.java
+++ src/test/java/sampleProject/TestProject1.java
@@ -8,14 +8,20 @@ public class TestProject1 {
     @Test(priority=1)
     public void test_login() {
 
-        System.out.println("Login Test Passed....");
+        System.out.println("Login Test Passed ...");
         Assert.assertTrue(true);
     }
 
     @Test(priority=2)
+    public void test_something() {
+
+        System.out.println("Testing Passed ...");
+        Assert.assertTrue(true);
+    }
+    @Test(priority=3)
     public void test_logout() {
 
-        System.out.println("Logout Test Passed....");
+        System.out.println("Logout Test Passed ...");
         Assert.assertTrue(true);
     }
 }
